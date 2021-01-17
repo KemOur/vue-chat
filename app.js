bdb2d0db342dd38869d23b79bbe746a050d1427d
Junior <duartejusanca@gmail.com>
2021-01-17 23:58:04 +0100

    all:ok

--- app.js
+++ app.js
@@ -1,30 +1,88 @@
 import notification from "./app/notification.js";
 
 const user ={
-    template: `
-               <li class="list-group-item"> <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="lightgreen"></rect></svg>Junior</li>
-               <li class="list-group-item"> <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="lightgreen"></rect></svg>Nathan</li>
-               <li class="list-group-item"><svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="lightgreen"></rect></svg>Thomas</li>`,
+    props: ['user'],
+    template: `<li class="list-group-item"><svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="lightgreen"></rect></svg>{{user.username}}</li>`,
 };
  const users ={
+     data() {
+         return {
+             user: {
+                 id: '',
+                 username: ''
+             },
+             users: [],
+         };
+     },
+     methods: {
+       UserConnected() {
+           const userCo = {
+               username: faker.name.findName()
+           };
+           this.users.unshift(userCo);
+           this.$emit('user', this.users.username);
+       },
+     },
+     mounted(){
+       setInterval(this.UserConnected, 2000);
+     },
      components: {
          User : user,
      },
      template: ` <div class="bg-light p-2 rounded">
                 <h5 style="text-align: center">Connected Users:</h5>
                 <ul class="list-group" style="width: 100%">
-                 <user></user>
+                 <user :user="user"
+                 v-for="user in users"></user>
                 </ul>
             </div>`,
  };
+
 const message ={
-    template: `<br><div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+    data() {
+        return{
+            message: {
+                id: Date.now,
+                username: '',
+                message:'',
+                date: '',
+
+            },
+            messages: []
+        }
+    },
+    methods: {
+        addNewTodo(){
+            const message = {
+                id: Date.now(),
+                username: 'user',
+                message: this.message.message,
+                date: new Date(Date.now()).toLocaleDateString(),
+            };
+            this.messages.push(message);
+            this.message.id = Date.now();
+            this.message.message='';
+            this.message.date='';
+
+        },
+    },
+    template: `<br>
+
+                <div v-for="message in messages" class="pb-3 mb-0 small lh-sm border-bottom w-100">
                             <div class="d-flex justify-content-between">
-                                <strong class="text-gray-dark">El Junioré</strong><em>17:55</em>
+                                <strong class="text-gray-dark">El Junioré</strong><em>{{message.date}}</em>
                             </div>
-                            <span class="d-block">@juju</span>
-                            <p class="lead">Salut les amigos, vous avez vû le matcht d'hier ?</p>
-              </div>`,
+                            <span class="d-block">@Juju</span><br>
+                            <p class="lead">{{message.message}}</p>
+              </div>
+              
+              <form @submit.prevent="addNewTodo">
+                  <div class="input-group mb-3">
+                    <input v-model="message.message" type="text" class="form-control" aria-describedby="basic-addon2">
+                    <span id="basic-addon2"  ><button class="btn btn-lg btn-info" type="submit" style="color: white" role="button">send</button></span>
+                </div>
+              </form>
+                   `,
 };
 const chat ={
     data() {
@@ -43,10 +101,6 @@ const chat ={
                             <h6 class="border-bottom pb-2 mb-0">{{ title }}</h6>
                             <message></message>
                     </div>
-                     <div class="input-group mb-3">
-                    <input type="text" class="form-control" aria-describedby="basic-addon2">
-                    <span id="basic-addon2"><a class="btn btn-lg btn-info" style="color: white" href="/docs/5.0/components/navbar/" role="button">send</a></span>
-                </div>
             </div>
         </div>`,
 
--- app/notification.js
+++ app/notification.js
@@ -2,16 +2,16 @@ setInterval(function(){
 
     faker.locale = 'fr'
     let info = faker.helpers.createCard();
-    let {username} = info;
+    let {name} = info;
 
-    document.querySelector('#name').value = username;
+    document.querySelector('#name').value = name;
 }, 2000);
 
-
-
 const notification ={
-    template: `<div class="alert alert-success" role="alert">@<input style="background: none; border-width: 0px; width: 120px; border: none" type="text" id="name"> vient d'entrer dans la salle !</div>
-`,
+    template: `<div class="alert alert-success" role="alert">
+                    @<input style="background: none; border-width: 0px; width: 120px; border: none" type="text" id="name"> 
+                    vient d'entrer dans la salle !
+                    </div>`,
 }
 
 export default notification;
